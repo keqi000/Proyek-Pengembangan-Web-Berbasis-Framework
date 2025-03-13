e1fbea7acd8cb77a3221ef9b350e03a7e9c4a516
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2025-03-13 14:02:05 +0800

    [UI] Set the `header` to not use `fixed`

---
+++ src/app/(generic)/perkuliahan/page.tsx
@@ -0,0 +1,7 @@
+export default function PerkuliahanPage(){
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+}
--- src/app/_layout/header.tsx
+++ src/app/_layout/header.tsx
@@ -1,7 +1,8 @@
 export default function Header(){
   return (
     // TODO: use Shadcn Avatar
-    <header className="bg-red-600 text-white p-4 flex justify-end items-center fixed top-0 left-0 w-full z-49">
+    <header className="bg-red-600 text-white p-4 flex justify-end items-center w-full">
+      
       <div className="flex items-center gap-4 mr-4">
         <span>FEBRIAN REZKI HEMETO</span>
         <img
